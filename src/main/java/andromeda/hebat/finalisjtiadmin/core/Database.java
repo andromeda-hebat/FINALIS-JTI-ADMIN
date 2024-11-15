be5819039e0af416dcd6450440357adebdd6f080
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-11-15 13:26:19 +0700

    add demo code for accessing sql server database

--- src/main/java/andromeda/hebat/finalisjtiadmin/core/Database.java
+++ src/main/java/andromeda/hebat/finalisjtiadmin/core/Database.java
@@ -1,16 +1,34 @@
 package andromeda.hebat.finalisjtiadmin.core;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Database {
 
-    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=YourDatabase";
-    private static final String USER = "your_username";
-    private static final String PASSWORD = "your_password";
+    private static final String URL = "jdbc:sqlserver://LAPTOP-V9Q55RPI;Database=db_belajar;encrypt=false";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "sem0gaBISA";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static String getUserData() {
+        StringBuilder data = new StringBuilder();
+
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT EmployeeID, FirstName, LastName FROM Employees")) {
+
+            while (resultSet.next()) {
+                int employeeID = resultSet.getInt("EmployeeID");
+                String firstName = resultSet.getString("FirstName");
+                String lastName = resultSet.getString("LastName");
+                data.append("ID: ").append(employeeID).append(", Name: ").append(firstName).append(lastName).append("\n");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error fetching data: " + e.getMessage());
+        }
+
+        return data.toString();
     }
 }
