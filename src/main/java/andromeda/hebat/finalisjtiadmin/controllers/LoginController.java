20a1973811172fcd5d4b322c45e2094e3fbaaba7
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-11-21 22:52:30 +0700

    add simple mechanism for user authentication

--- src/main/java/andromeda/hebat/finalisjtiadmin/controllers/LoginController.java
+++ src/main/java/andromeda/hebat/finalisjtiadmin/controllers/LoginController.java
@@ -1,4 +1,34 @@
 package andromeda.hebat.finalisjtiadmin.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    @FXML
+    private TextField inputFieldUsername;
+
+    @FXML
+    private PasswordField inputFieldPassword;
+
+    @FXML
+    private void enterDashboard() {
+        String username = inputFieldUsername.getText();
+        String password = inputFieldPassword.getText();
+
+        switch (username) {
+            case "adminjurusan":
+                System.out.println(password.equals("jurusan") ? "Admin jurusan valid!" : "Admin jurusan tidak valid!");
+                break;
+            case "adminprodi":
+                System.out.println(password.equals("prodi") ? "Admin prodi valid!" : "Admin prodi tidak valid!");
+                break;
+            case "adminta":
+                System.out.println(password.equals("tugasakhir") ? "Admin TA valid!" : "Admin TA tidak valid!");
+                break;
+            default:
+                System.out.println("Tidak ditemukan username yang cocok dengan data pengguna!");
+                break;
+        }
+    }
 }
