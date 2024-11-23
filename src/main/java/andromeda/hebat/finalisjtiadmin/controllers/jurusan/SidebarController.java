75325e317df795d58a8f2f18b198e5702d8e2b66
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-11-23 15:02:27 +0700

    add javafx id for some .fxml element and add controller for sidebar

--- src/main/java/andromeda/hebat/finalisjtiadmin/controllers/jurusan/SidebarController.java
+++ src/main/java/andromeda/hebat/finalisjtiadmin/controllers/jurusan/SidebarController.java
@@ -1,4 +1,70 @@
 package andromeda.hebat.finalisjtiadmin.controllers.jurusan;
 
+import andromeda.hebat.finalisjtiadmin.helper.SceneHelper;
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.List;
+
 public class SidebarController {
+
+    private Stage mainStage;
+
+    @FXML
+    private VBox sidebarJurusan;
+
+    @FXML
+    private Button btnKelolaData;
+
+    @FXML
+    private Button btnAdmin;
+
+    @FXML
+    private Button btnMahasiswa;
+
+    @FXML
+    private Button btnTemplateSurat;
+
+    @FXML
+    private Button btnLogAktivitas;
+
+    @FXML
+    private Button btnLaporan;
+
+    @FXML
+    public void initialize() {
+        List<Button> buttons = List.of(
+                btnKelolaData,
+                btnAdmin,
+                btnMahasiswa,
+                btnTemplateSurat,
+                btnLogAktivitas,
+                btnLaporan
+        );
+
+        for (Button button : buttons) {
+            addMouseEvents(button);
+        }
+    }
+
+    private void addMouseEvents(Button button) {
+        button.setOnMouseEntered(event -> button.setCursor(Cursor.HAND));
+        button.setOnMousePressed(event -> button.setCursor(Cursor.DEFAULT));
+        button.setOnMouseReleased(event -> button.setCursor(Cursor.HAND));
+    }
+
+    @FXML
+    public void btnAdminOnClicked() {
+        mainStage = (Stage) sidebarJurusan.getScene().getWindow();
+        SceneHelper.changeScene(mainStage, "/views/adminjurusan/kelola-data-admin.fxml");
+    }
+
+    @FXML
+    public void btnTemplateSuratOnClicked() {
+        mainStage = (Stage) sidebarJurusan.getScene().getWindow();
+        SceneHelper.changeScene(mainStage, "/views/adminjurusan/kelola-template-surat.fxml");
+    }
 }
