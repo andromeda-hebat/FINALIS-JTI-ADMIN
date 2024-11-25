fbcf223d541fa47c611fc1e87e0d6d09525cd31c
Stevan Zaky <stevanzaky87@gmail.com>
2024-11-25 15:30:53 +0700

    menambahkan overlay tambah, edit, hapus admin

--- src/main/java/andromeda/hebat/finalisjtiadmin/controllers/jurusan/KelolaDataAdminController.java
+++ src/main/java/andromeda/hebat/finalisjtiadmin/controllers/jurusan/KelolaDataAdminController.java
@@ -1,5 +1,55 @@
 package andromeda.hebat.finalisjtiadmin.controllers.jurusan;
 
+import andromeda.hebat.finalisjtiadmin.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class KelolaDataAdminController {
+    @FXML
+    private Button btnTambahkan;
+
+    public void openOverlayTambahAdmin(){
+        try {
+            Stage overlay = new Stage();
+            overlay.setTitle("Tambah Template Surat Baru");
+
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/adminjurusan/overlay-tambah-admin.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 500, 600);
+            overlay.setScene(scene);
+            overlay.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void openOverlayEditAdmin(){
+        try {
+            Stage overlay = new Stage();
+            overlay.setTitle("Edit Data Admin");
+
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/adminjurusan/overlay-edit-data-admin.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 500, 600);
+            overlay.setScene(scene);
+            overlay.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void openOverlayDeleteAdmin(){
+        try {
+            Stage overlay = new Stage();
+            overlay.setTitle("Apakah Anda ingin Menghapus data ini");
+
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/adminjurusan/overlay-hapus-data-admin.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 300, 400);
+            overlay.setScene(scene);
+            overlay.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
