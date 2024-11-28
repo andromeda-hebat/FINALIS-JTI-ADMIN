beab0e47eeea416dc5f0eb78abc2f7034eed51c8
DewitaA12 <xsclthzn@gmail.com>
2024-11-28 11:33:22 +0700

    Menambahkan halaman admin prodi

---
+++ src/main/java/andromeda/hebat/finalisjtiadmin/controllers/jurusan/apaaan.java
@@ -0,0 +1,5 @@
+package andromeda.hebat.finalisjtiadmin.controllers.jurusan;
+
+public class apaaan {
+    andromeda.hebat.finalisjtiadmin.controllers.jurusan.apaaan;
+}
--- src/main/java/andromeda/hebat/finalisjtiadmin/core/Database.java
+++ src/main/java/andromeda/hebat/finalisjtiadmin/core/Database.java
@@ -4,10 +4,10 @@ import java.sql.*;
 
 public class Database {
 
-    private static final String SERVER_NAME = "LAPTOP-V9Q55RPI";
+    private static final String SERVER_NAME = "DESKTOP-6MN37MN\\BDL";
     private static final String DB_NAME = "db_temp_finalis_jti";
-    private static final String USER = "sa";
-    private static final String PASSWORD = "sem0gaBISA";
+    private static final String USER = "dewita";
+    private static final String PASSWORD = "Dewita12";
 
     public static Connection getConnection() {
         final String URL = "jdbc:sqlserver://"+SERVER_NAME+";Database="+DB_NAME+";encrypt=false";
