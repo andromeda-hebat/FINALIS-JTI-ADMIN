10a9a07e8b1fa083088e29dc31148e92bd1d79ae
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-11-29 13:35:01 +0700

    add logic to change scene for sidebar admin ta and admin prodi

--- src/main/java/andromeda/hebat/finalisjtiadmin/controllers/prodi/SidebarController.java
+++ src/main/java/andromeda/hebat/finalisjtiadmin/controllers/prodi/SidebarController.java
@@ -2,15 +2,50 @@ package andromeda.hebat.finalisjtiadmin.controllers.prodi;
 
 import andromeda.hebat.finalisjtiadmin.helper.SceneHelper;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.List;
+
 public class SidebarController {
+
     private Stage mainStage;
+
+    @FXML private VBox sidebarContainer;
+
+    @FXML private  Button btnDashboard;
+
+    @FXML private Button btnRequestVerification;
+
     @FXML
-    private VBox sidebar;
-    public void btnPermintaanVerifSeluruhOnClicked() {
-        mainStage = (Stage) sidebar.getScene().getWindow();
+    public void initialize() {
+        List<Button> buttons = List.of(
+                btnDashboard,
+                btnRequestVerification
+        );
+
+        for (Button button : buttons) {
+            addMouseEvents(button);
+        }
+    }
+
+    private void addMouseEvents(Button button) {
+        button.setOnMouseEntered(event -> button.setCursor(Cursor.HAND));
+        button.setOnMousePressed(event -> button.setCursor(Cursor.DEFAULT));
+        button.setOnMouseReleased(event -> button.setCursor(Cursor.HAND));
+    }
+
+    @FXML
+    public void btnDashboardOnClicked() {
+        Stage mainStage = (Stage) sidebarContainer.getScene().getWindow();
+        SceneHelper.changeScene(mainStage, "/views/adminprodi/dashboard.fxml");
+    }
+
+    @FXML
+    public void btnRequestVerificationOnClicked() {
+        mainStage = (Stage) sidebarContainer.getScene().getWindow();
         SceneHelper.changeScene(mainStage, "/views/adminprodi/permintaan-verif-seluruh.fxml");
     }
 }
--- src/main/java/andromeda/hebat/finalisjtiadmin/controllers/ta/SidebarController.java
+++ src/main/java/andromeda/hebat/finalisjtiadmin/controllers/ta/SidebarController.java
@@ -1,4 +1,51 @@
 package andromeda.hebat.finalisjtiadmin.controllers.ta;
 
+import andromeda.hebat.finalisjtiadmin.helper.SceneHelper;
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.List;
+
 public class SidebarController {
+
+    private Stage mainStage;
+
+    @FXML private VBox sidebarContainer;
+
+    @FXML private Button btnDashboard;
+
+    @FXML private Button btnRequestVerification;
+
+    @FXML
+    public void initialize() {
+        List<Button> buttons = List.of(
+                btnDashboard,
+                btnRequestVerification
+        );
+
+        for (Button button : buttons) {
+            addMouseEvents(button);
+        }
+    }
+
+    private void addMouseEvents(Button button) {
+        button.setOnMouseEntered(event -> button.setCursor(Cursor.HAND));
+        button.setOnMousePressed(event -> button.setCursor(Cursor.DEFAULT));
+        button.setOnMouseReleased(event -> button.setCursor(Cursor.HAND));
+    }
+
+    @FXML
+    public void btnDashboardOnClicked() {
+        Stage mainStage = (Stage) sidebarContainer.getScene().getWindow();
+        SceneHelper.changeScene(mainStage, "/views/adminta/dashboard.fxml");
+    }
+
+    @FXML
+    public void btnRequestVerificationOnClicked() {
+        mainStage = (Stage) sidebarContainer.getScene().getWindow();
+        SceneHelper.changeScene(mainStage, "/views/adminta/permintaan-verif-seluruh.fxml");
+    }
 }
