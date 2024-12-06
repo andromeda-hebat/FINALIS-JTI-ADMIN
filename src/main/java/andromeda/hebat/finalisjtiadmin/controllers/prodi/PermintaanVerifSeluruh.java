5e5b2fd764ecd51084cf83324501ce714340bfcb
Stevan Zaky <stevanzaky87@gmail.com>
2024-12-06 15:23:05 +0700

    mengambil data permintaan verifikasi prodi dari database

--- src/main/java/andromeda/hebat/finalisjtiadmin/controllers/prodi/PermintaanVerifSeluruh.java
+++ src/main/java/andromeda/hebat/finalisjtiadmin/controllers/prodi/PermintaanVerifSeluruh.java
@@ -1,26 +1,32 @@
 package andromeda.hebat.finalisjtiadmin.controllers.prodi;
+import andromeda.hebat.finalisjtiadmin.Main;
 import andromeda.hebat.finalisjtiadmin.models.BerkasProdi;
 import andromeda.hebat.finalisjtiadmin.core.Database;
 import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class PermintaanVerifSeluruh {
+    @FXML private HBox halamanPermintaanVerif;
     @FXML
     private Label judulHalaman;
 
     @FXML private TableView<BerkasProdi> tableViewBerkasProdi;
 
-    @FXML private TableColumn<BerkasProdi, Integer> numberColumn;
+    @FXML private TableColumn<BerkasProdi, String> numberColumn;
 
     @FXML private TableColumn<BerkasProdi, String> idColumn;
 
@@ -40,10 +46,11 @@ public class PermintaanVerifSeluruh {
         berkasProdiList = FXCollections.observableArrayList();
         getAllBerkasProdi();
 
-        numberColumn.setCellValueFactory((TableColumn.CellDataFeatures<BerkasProdi, Integer> cellData) -> {
-            int index = tableViewBerkasProdi.getItems().indexOf(cellData.getValue()) + 1;
-            return new ReadOnlyObjectWrapper<>(index);
-        });
+//        numberColumn.setCellValueFactory((TableColumn.CellDataFeatures<BerkasProdi, Integer> cellData) -> {
+//            int index = tableViewBerkasProdi.getItems().indexOf(cellData.getValue()) + 1;
+//            return new ReadOnlyObjectWrapper<>(index);
+//        });
+        numberColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getIdBerkasProdi()));
         idColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNim()));
         nameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getNamaMahasiswa()));
         descriptionColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getKeterangan()));
@@ -54,7 +61,7 @@ public class PermintaanVerifSeluruh {
             {
                 detailBtn.setOnAction(event -> {
                     BerkasProdi BerkasProdi = getTableView().getItems().get(getIndex());
-                    System.out.println("detail button telah diklik");
+                    openHalamanDetail();
                 });
             }
 
@@ -90,6 +97,19 @@ public class PermintaanVerifSeluruh {
 
                 berkasProdiList.add(new BerkasProdi(nim, mahasiswa, keterangan, tanggalRequest));
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void openHalamanDetail() {
+        try {
+            Stage overlay = (Stage) halamanPermintaanVerif.getScene().getWindow();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/adminprodi/detail-permintaan-verif.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 500, 600);
+            overlay.setScene(scene);
+            overlay.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
