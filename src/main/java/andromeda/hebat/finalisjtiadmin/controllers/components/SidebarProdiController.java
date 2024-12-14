a07d2155717f088b5bfeb5ede66681e378318d82
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-15 06:29:03 +0700

    remove unecesssary java class

--- src/main/java/andromeda/hebat/finalisjtiadmin/controllers/components/SidebarProdiController.java
+++ src/main/java/andromeda/hebat/finalisjtiadmin/controllers/components/SidebarProdiController.java
@@ -2,12 +2,9 @@ package andromeda.hebat.finalisjtiadmin.controllers.components;
 
 import andromeda.hebat.finalisjtiadmin.helper.SceneHelper;
 import javafx.fxml.FXML;
-import javafx.scene.Cursor;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 
 public class SidebarProdiController extends SidebarController {
-    @FXML private VBox sidebarContainer;
     @FXML private  Button btnDashboard;
     @FXML private Button btnRequestVerification;
 
