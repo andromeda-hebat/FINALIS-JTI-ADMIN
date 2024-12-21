769221cadca1eef55833e5d3f60516105ef52607
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-21 21:02:39 +0700

    fix: incorrect variable name in sidebar jurusan

--- src/main/java/andromeda/hebat/finalisjtiadmin/controllers/components/admin/jurusan/SidebarJurusanController.java
+++ src/main/java/andromeda/hebat/finalisjtiadmin/controllers/components/admin/jurusan/SidebarJurusanController.java
@@ -3,16 +3,9 @@ package andromeda.hebat.finalisjtiadmin.controllers.components.admin.jurusan;
 import andromeda.hebat.finalisjtiadmin.controllers.components.admin.SidebarController;
 import andromeda.hebat.finalisjtiadmin.helper.SceneHelper;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import java.util.Optional;
 
 public class SidebarJurusanController extends SidebarController {
-    @FXML private VBox sidebarJurusan;
     @FXML private Button btnDashboard;
     @FXML private Button btnKelolaData;
     @FXML private Button btnAdmin;
@@ -26,26 +19,26 @@ public class SidebarJurusanController extends SidebarController {
 
     @FXML
     private void btnDashboardOnClicked() {
-        SceneHelper.changeRootNodeScene(sidebarJurusan.getScene(), "/views/pages/admin/jurusan/dashboard.fxml");
+        SceneHelper.changeRootNodeScene(sidebarContainer.getScene(), "/views/pages/admin/jurusan/dashboard.fxml");
     }
 
     @FXML
     private void btnAdminOnClicked() {
-        SceneHelper.changeRootNodeScene(sidebarJurusan.getScene(), "/views/pages/admin/jurusan/kelola-data-admin.fxml");
+        SceneHelper.changeRootNodeScene(sidebarContainer.getScene(), "/views/pages/admin/jurusan/kelola-admin.fxml");
     }
 
     @FXML
     private void btnTemplateSuratOnClicked() {
-        SceneHelper.changeRootNodeScene(sidebarJurusan.getScene(), "/views/pages/admin/jurusan/kelola-template-surat.fxml");
+        SceneHelper.changeRootNodeScene(sidebarContainer.getScene(), "/views/pages/admin/jurusan/kelola-template-surat.fxml");
     }
 
     @FXML
     private void btnMahasiswaOnClicked() {
-        SceneHelper.changeRootNodeScene(sidebarJurusan.getScene(), "/views/pages/admin/jurusan/kelola-data-mahasiswa.fxml");
+        SceneHelper.changeRootNodeScene(sidebarContainer.getScene(), "/views/pages/admin/jurusan/kelola-mahasiswa.fxml");
     }
 
     @FXML
     private void btnLogAktivitasOnClicked() {
-        SceneHelper.changeRootNodeScene(sidebarJurusan.getScene(), "/views/pages/admin/jurusan/log-aktivitas.fxml");
+        SceneHelper.changeRootNodeScene(sidebarContainer.getScene(), "/views/pages/admin/jurusan/log-aktivitas.fxml");
     }
 }
